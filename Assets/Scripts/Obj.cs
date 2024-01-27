10b523d3c35e06707d94ff00255e1a99f3e4e927
Jasminezhang666666 <jz5144@nyu.edu>
2024-01-27 22:43:55 +0800

    sth

--- Assets/Scripts/Obj.cs
+++ Assets/Scripts/Obj.cs
@@ -5,14 +5,43 @@ public class Obj : MonoBehaviour
     public bool isLifted = false;
     public bool isFalling = false;
 
+    private void Start()
+    {
+        UpdateCollisionSettings();
+    }
+
     private void OnCollisionStay2D(Collision2D collision)
     {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            cancelParent();
+            return;
+        }
+
         if (isFalling && !collision.gameObject.CompareTag("Feet"))
         {
-            print("isFalling set to false.");
             isFalling = false; // Reset isFalling when colliding with other objects
-
         }
     }
 
+    private void UpdateCollisionSettings()
+    {
+        // If the Obj is on a Van, ignore collisions with the default layer
+        if (transform.parent != null && transform.parent.gameObject.layer == LayerMask.NameToLayer("Van"))
+        {
+            Physics2D.IgnoreLayerCollision(gameObject.layer, LayerMask.NameToLayer("Default"), true);
+        }
+        else
+        {
+            print("Not ignoring default layer anymore.");
+            // Reset collision settings to default when not a child of Van
+            Physics2D.IgnoreLayerCollision(gameObject.layer, LayerMask.NameToLayer("Default"), false);
+        }
+    }
+
+    public void cancelParent()
+    {
+        transform.parent = null;
+        UpdateCollisionSettings(); 
+    }
 }

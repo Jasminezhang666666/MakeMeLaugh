7b90f24258be7316c6895a2c05574b7a6004b045
Pigeon-hcy <129189900+Pigeon-hcy@users.noreply.github.com>
2024-01-27 16:41:23 -0500

    CY change

---
+++ Assets/Scripts/ConeGun.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConeGun : MonoBehaviour
+{
+    public int coolDown;
+    public int coolCounter;
+    public GameObject bullet;
+    public bool canFire;
+    public Rigidbody2D rb;
+    public float shootRate;
+    public AudioSource shootSound;
+ 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        coolCounter = coolDown;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canFire)
+        {
+            coolCounter--;
+        }
+        if (coolCounter == 0)
+        {
+
+            StartCoroutine(MafiaFireGun());
+            coolCounter = coolDown;
+        }
+    }
+
+    IEnumerator MafiaFireGun()
+    {
+        Instantiate(bullet, rb.position, Quaternion.identity);
+        shootSound.Play();
+        yield return new WaitForSeconds(shootRate);
+
+
+
+
+    }
+}

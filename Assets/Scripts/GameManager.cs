7340b797c81f14728e57b981e2b7634635431723
bellinimoon <98369237+bellinimoon@users.noreply.github.com>
2024-01-28 12:45:41 -0500

    finished start

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private bool isEnd = false;
+    private GameObject end;
+    private GameObject start;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
     void Start()
     {
+        end = GameObject.Find("End");
+        start = GameObject.Find("Start");
+        ChangeToStart();
         //GameObject.Find("Pizza").GetComponent<pizza>().Launch();
     }
 
+    public void PlayVideoScene()
+    {
+        SceneManager.LoadScene("Start2");
+    }
 
+    public void PlayGameScene()
+    {
+        SceneManager.LoadScene("LevelScene");
+    }
+    public void PlayEndScene()
+    {
+        SceneManager.LoadScene("StartAndFinish");
+        isEnd = true;
+        ChangeToEnd();
 
+    }
+
+    public void Reset()
+    {
+        isEnd = false;
+        ChangeToStart();
+    }
+    private void ChangeToEnd()
+    {
+        start.SetActive(false);
+        end.SetActive(true);
+    }
+
+    private void ChangeToStart()
+    {
+        start.SetActive(true);
+        end.SetActive(false);
+    }
 
 }

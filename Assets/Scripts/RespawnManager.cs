117dc7ea975981fd2f1e5a9f0d1f676fd176160b
Jasminezhang666666 <jz5144@nyu.edu>
2024-01-28 20:18:09 +0800

    Spawning System completed!!

--- Assets/Scripts/RespawnManager.cs
+++ Assets/Scripts/RespawnManager.cs
@@ -7,7 +7,9 @@ public class RespawnManager : MonoBehaviour
     [SerializeField] private int maxObjects = 5;
     [SerializeField] private List<GameObject> movingObjectPrefabs;
     [SerializeField] private List<GameObject> nonMovingObjectPrefabs;
-    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private Transform movingRespawnPoint;
+    [SerializeField] private Transform nonMovingRespawnArea;
+    [SerializeField] private Vector2 nonMovingAreaSize = new Vector2(5f, 5f);
 
     private Collider2D areaCollider;
     private Dictionary<GameObject, Vector3> originalPositions = new Dictionary<GameObject, Vector3>();
@@ -20,16 +22,8 @@ public class RespawnManager : MonoBehaviour
             Debug.LogError("RespawnManager requires a Collider2D component on the GameObject.");
         }
 
-        // Store the original positions of all objects
+        // Store the original positions of the moving objects
         foreach (var obj in movingObjectPrefabs)
-        {
-            if (obj != null)
-            {
-                originalPositions[obj] = obj.transform.position;
-            }
-        }
-
-        foreach (var obj in nonMovingObjectPrefabs)
         {
             if (obj != null)
             {
@@ -75,17 +69,24 @@ public class RespawnManager : MonoBehaviour
 
                 if (movingObjectPrefabs.Contains(prefab))
                 {
-                    // If it's a moving object, maintain its original Y position
-                    spawnPosition = new Vector3(respawnPoint.position.x, originalPositions[prefab].y, respawnPoint.position.z);
+                    // For moving objects, use the moving respawn point
+                    spawnPosition = new Vector3(movingRespawnPoint.position.x, originalPositions[prefab].y, movingRespawnPoint.position.z);
                 }
                 else
                 {
-                    // If it's a non-moving object, use its original position
-                    spawnPosition = originalPositions[prefab];
+                    // For non-moving objects, respawn within the non-moving area
+                    spawnPosition = GetRandomPositionInArea(nonMovingRespawnArea.position, nonMovingAreaSize);
                 }
 
                 Instantiate(prefab, spawnPosition, Quaternion.identity);
             }
         }
+    }
+
+    private Vector3 GetRandomPositionInArea(Vector3 center, Vector2 size)
+    {
+        float randomX = Random.Range(center.x - size.x / 2, center.x + size.x / 2);
+        float randomY = Random.Range(center.y - size.y / 2, center.y + size.y / 2);
+        return new Vector3(randomX, randomY, center.z);
     }
 }
